6f09e4e9bcc196c035f0a512ac4e46c61ae1cbd5
azime12 <azimerawtaddese75@gmail.com>
2024-06-07 20:06:04 +0300

    Add model file tut3-model.pt using Git LFS

--- app.py
+++ app.py
@@ -154,7 +154,6 @@ def translate():
     return jsonify({'translation': ' '.join(translation)})
 
 # Run the app
-if __name__ == '__main__':
-    # Run the app on port 8080
-    app.run(host='0.0.0.0', port=100000, debug=True)
+f __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
