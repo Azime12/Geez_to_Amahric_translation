5c7b38c9f3b84c4a98247e96d4c1c788e9478fe5
azime12 <azimerawtaddese75@gmail.com>
2024-06-07 17:27:58 +0300

    Add model file tut3-model.pt using Git LFS

--- app.py
+++ app.py
@@ -155,6 +155,6 @@ def translate():
 
 # Run the app
 if __name__ == '__main__':
-    # Run the app, Flask will automatically detect the available port
-    app.run(host='0.0.0.0', debug=True)
+    # Run the app on port 8080
+    app.run(host='0.0.0.0', port=100000, debug=True)
 
